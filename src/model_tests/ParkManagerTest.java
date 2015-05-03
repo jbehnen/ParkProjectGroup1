f7fed4061bc9bc4cf938fc7aca88021615924f9a
Julia Behnen <jmbehnen@gmail.com>
2015-05-02 18:52:32 -0700

    Finished first version of all user classes and associated tests.

--- src/model_tests/ParkManagerTest.java
+++ src/model_tests/ParkManagerTest.java
@@ -2,18 +2,38 @@ package model_tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import park_model.ParkManager;
+
 public class ParkManagerTest {
+	
+	private static final String DEFAULT_EMAIL = "smith@aol.com";
+	
+	private ParkManager myPM;
+	private List<String> myParks;
 
 	@Before
 	public void setUp() throws Exception {
+		myParks = new ArrayList<String>();
+		myParks.add("Park");
+		myPM = new ParkManager(DEFAULT_EMAIL, myParks);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testParkManagerConstructor() {
+		assertEquals("Email should be the same", DEFAULT_EMAIL, myPM.getEmail());
+		assertEquals("Park list contents should be the same", myParks, myPM.getParks());
+		assertNotSame("List should be a copy", myParks, myPM.getParks());
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testGetParksShouldReturnUnmodifiableList() {
+		myPM.getParks().add("Failure Park");
 	}
 
 }
--- src/model_tests/UserTest.java
+++ src/model_tests/UserTest.java
@@ -2,18 +2,38 @@ package model_tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import park_model.ParkManager;
+import park_model.User;
+import park_model.Volunteer;
+
 public class UserTest {
+	
+	private static final String DEFAULT_EMAIL = "smith@aol.com";
+	private static final String DEFAULT_LAST_NAME = "Smith";
+	private User myUser;
 
 	@Before
 	public void setUp() throws Exception {
+		myUser = new Volunteer(DEFAULT_EMAIL, DEFAULT_LAST_NAME);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testVolunteerConstructor() {
+		assertEquals("Email should be set", DEFAULT_EMAIL, myUser.getEmail());
+	}
+	
+	@Test
+	public void testUserEquals() {
+		User otherUser = new Volunteer(DEFAULT_EMAIL, "Jones");
+		assertEquals("Email is only parameter needed for equality", myUser, otherUser);
+		otherUser = new Volunteer("jones@aol.com", DEFAULT_LAST_NAME);
+		assertNotEquals("Last name does not guarantee equality", myUser, otherUser);
 	}
 
 }
