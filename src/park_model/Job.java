bbc18c2164505b2dcc09875b6674b6df6af96388
lahama9 <lahama9@Lisas-MacBook-Pro.local>
2015-05-02 21:36:55 -0700

    Untested Code for Job.java

--- src/park_model/Job.java
+++ src/park_model/Job.java
@@ -1,57 +1,96 @@
 package park_model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 public class Job {
 	
+	//Class Variables
+	private String myPark;
+	private List<Date> myDates;
+	private List<Volunteer> myVolunteers;
+	private int numLightJobs;
+	private int numMediumJobs;
+	private int numHeavyJobs;
+
+	/**
+	 * Class Constructor
+	 * Creates an instance of a Job.
+	 * @param thePark		the location of a job as a park
+	 * @param theDates		the date(s) of a job
+	 * @param theLightNum	the number of jobs in the work category Light that is available.
+	 * @param theMediumNum	the number of jobs in the work category Medium that is available.
+	 * @param theHeavyNum	the number of jobs in the work category Heavy that is available. 
+	 */
 	public Job(String thePark, List<Date> theDates, int theLightNum, int theMediumNum, int theHeavyNum) {
+		myPark = thePark;
+		numLightJobs = theLightNum;
+		numMediumJobs = theMediumNum;
+		numHeavyJobs = theHeavyNum;
+
+		myDates = new ArrayList<Date>();
 		
+		for(int i= 0; i< theDates.size(); i++){
+			myDates.add(theDates.get(i));
+		}
 	}
 	
+	/**
+	 * Copy Constructor
+	 * Creates a copy of a job passed in as a parameter.
+	 * @param theJob	the job to be copied.
+	 */
+	
 	public Job(Job theJob) {
-		// we need a copy constructor for JobSchedule
+		//Copy of Dates	
+		List<Date> thisDate = new ArrayList<Date>();
+		for(Date d : myDates){
+			thisDate.add(d);
+		}
+		
+		//Calls class constructor.
+		Job thisJob = new Job(myPark, thisDate, numLightJobs, numMediumJobs, numHeavyJobs);
 	}
 	
 	/**
 	 * Returns an unmodifiable list of the date(s) during which this job takes place.
-	 * 
-	 * @return
-	 * 
-	 * Delete the following note when you are done:
-	 * See http://docs.oracle.com/javase/7/docs/api/java/util/Collections.html#unmodifiableList(java.util.List)
+	 * @return unmodifiable list of dates for a job.
 	 */
 	public List<Date> getDates() {
-		return null;
-		
+		return Collections.unmodifiableList(myDates);
 	}
 	
 	/**
 	 * Returns an unmodifiable list of the work categories that still have slots available for sign-up.
-	 * 
-	 * @return
-	 */
+	 * @return a list of Work Categories still available. 
+	*/ 
+	 
 	public List<WorkCategory> getAvailableWorkCategories() {
-		return null;
+		List<WorkCategory> wcList = new ArrayList<WorkCategory>();
+		
+		//Checks each Work Category for availability.
+		if(numLightJobs >0){
+			wcList.add(WorkCategory.LIGHT);
+		}
+		if(numMediumJobs > 0){
+			wcList.listIterator().add(WorkCategory.MEDIUM);
+		}
+		if(numHeavyJobs > 0){
+			wcList.add(WorkCategory.HEAVY);
+		}
+
+		return Collections.unmodifiableList(wcList);
 	}
 	
 	/**
 	 * Returns an unmodifiable list of the volunteers signed up for the job.
 	 * 
-	 * @return
+	 * @return a list of volunteers 
 	 */
 	public List<Volunteer> getVolunteers() {
-		return null;
-	}
-	
-	/**
-	 * Returns true if theVolunteer is signed up for the job, false otherwise.
-	 * 
-	 * @param theVolunteer
-	 * @return
-	 */
-	public boolean isSignedUp(Volunteer theVolunteer) {
-		return false;
+		return Collections.unmodifiableList(myVolunteers);
 	}
 	
 	/**
@@ -59,10 +98,65 @@ public class Job {
 	 * of business rules.
 	 * 
 	 * @param theVolunteer
-	 * @return
+	 * @return boolean value
 	 */
 	public boolean signUp(Volunteer theVolunteer, WorkCategory theCategory) {
-		return false;
+		
+		boolean signUp = true;
+		signUp = isSignedUp(theVolunteer);
+		
+		//Checks to see if their work category is available.
+		if(signUp){
+			switch (theCategory){
+			case LIGHT:
+				if(numLightJobs <= 0){
+					signUp = false;
+				} else{
+					numLightJobs--;
+				}
+				break;
+			case MEDIUM:
+				if(numMediumJobs <= 0){
+					signUp = false;
+				} else{
+					numMediumJobs--;
+				}
+				break;
+			case HEAVY:
+				if(numHeavyJobs <= 0){
+					signUp = false;
+				}else{
+					numHeavyJobs--;
+				}
+				break;
+			}
+		}
+		//Add volunteer to Job list if signedUp is still true
+		if(signUp){
+			myVolunteers.add(theVolunteer);
+		}
+		return signUp;
 	}
 	
+	/**
+	 * Returns true if theVolunteer is signed up for the job, false otherwise.
+	 * 
+	 * @param theVolunteer
+	 * @return boolean 
+	 */
+	public boolean isSignedUp(Volunteer theVolunteer) {
+		boolean isSignedUp = false;
+		int count = 0;
+		
+		while(!(isSignedUp) && (count < myVolunteers.size())){
+			User v = (Volunteer) myVolunteers.get(count);
+			if(v.getEmail().equals(theVolunteer.getEmail())){
+				isSignedUp = false;
+			}else{
+				isSignedUp = true;
+			}
+			count++;
+		}
+		return isSignedUp;
+	}
 }
