41d29c2701cf866975f902580d7404e90d6635dd
lee <leebui99@uw.edu>
2015-05-03 17:10:10 -0700

    First version of VolunteerList Class and Test Class of its

--- src/model_tests/VolunteerListTest.java
+++ src/model_tests/VolunteerListTest.java
@@ -5,15 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class VolunteerListTest {
+import park_model.VolunteerList;
 
+
+public class VolunteerListTest {
+	private static final String LastName = "Smith";
+	private VolunteerList myVolunteerList;
 	@Before
 	public void setUp() throws Exception {
+		myVolunteerList = new VolunteerList();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals("List of Volunteer Last Name should be set", LastName,
+				myVolunteerList.getVolunteersByLastName(LastName));
 	}
 
 }
--- src/park_model/VolunteerList.java
+++ src/park_model/VolunteerList.java
@@ -1,35 +1,69 @@
 package park_model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VolunteerList {
+	
+	private List<Volunteer> list;
+	private String myLastName;
 	
 	/**
 	 * Constructs a list of all volunteers in the system from the back-end data.
 	 */
 	public VolunteerList() {
 		
+		List<Volunteer> list = new ArrayList<Volunteer>();
+		Volunteer theLastName = null;
+		list.add(theLastName);
+		
 	}
 	
 	/**
-	 * 
-	 * 
+	 *	Class Constructor.
+	 * 	Creates an instance of Job.
 	 * @param theLastName The last name being searched for.
 	 * @return An unmodifiable list of volunteers with the last name theLastName. 
 	 * 
-	 * Delete the following note when you are done:
-	 * See http://docs.oracle.com/javase/7/docs/api/java/util/Collections.html#unmodifiableList(java.util.List)
 	 */
-	public List<Volunteer> getVolunteersByLastName(String theLastName) {
-		return null;
+	public List<Volunteer> getVolunteersByLastName(String theLastName) {   
+		myLastName = theLastName;
 		
+		return Collections.unmodifiableList(list);	
 	}
 	
 	/**
 	 * Writes all list information to the back-end storage.
 	 */
 	public void saveList() {
-		// don't do this yet?
+		
+		//create a input file of volunteer list
+		Scanner input = null; 
+	    try
+	    { 
+	        input = new Scanner(new File("VolunteerList/volunteerList.txt")); 
+	          
+	        while (input.hasNextLine()) 
+	        { 
+	            String lastName = input.nextLine();
+	            System.out.println(lastName);   
+	        } 
+	    } 
+	    catch (FileNotFoundException ex) 
+	    { 
+	        System.out.println(ex.getMessage()); 
+	    } 
+	    finally
+	    { 
+	        if (input != null) 
+	        { 
+	            input.close(); 
+	        } 
+	    }
 	}
 	
 }
