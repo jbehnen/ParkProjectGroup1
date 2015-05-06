8fa94c1086c4d53d70010771113d6a7bb3dca7e3
Julia Behnen <jmbehnen@gmail.com>
2015-05-06 10:08:51 -0700

    Created the DateAndTime class to hold all time information about a job on a given date. Needs more documentation. Uses Gregorian Calendar instead of date. Can continue using JobSchedule as before so long as Job.getDates() returns the date extracted from this class. Job should now hold list of DateAndTime rather than Date

---
+++ src/park_model/DateAndTime.java
@@ -0,0 +1,50 @@
+package park_model;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * An immutable class that describes the date, start time, and end time of an event, 
+ * with the date set to the morning midnight of that date.
+ * 
+ * @author Julia Behnen
+ * @version 5/6/2015
+ *
+ */
+public class DateAndTime implements Comparable<DateAndTime> {
+	
+	private final GregorianCalendar myDate;
+	private final String myStartTime;
+	private final String myEndTime;
+	
+	public DateAndTime(int theYear, int theMonth, int theDate, String myStartTime, String myEndTime) {
+		myDate = new GregorianCalendar(theYear, theMonth, theDate);
+		this.myStartTime = myStartTime;
+		this.myEndTime = myEndTime;
+	}
+	
+	public DateAndTime(DateAndTime theJobDate) {
+		this(theJobDate.myDate.get(Calendar.YEAR), theJobDate.myDate.get(Calendar.MONTH), 
+				theJobDate.myDate.get(Calendar.DATE), theJobDate.myStartTime, 
+				theJobDate.myEndTime);
+	}
+	
+	public GregorianCalendar getDate() {
+		return (GregorianCalendar) myDate.clone();
+	}
+	
+	public String getStartTime() {
+		return myStartTime;
+	}
+	
+	public String getEndTime() {
+		return myEndTime;
+	}
+
+	@Override
+	public int compareTo(DateAndTime o) {
+		return this.myDate.compareTo(o.myDate);
+	}
+	
+	
+}
